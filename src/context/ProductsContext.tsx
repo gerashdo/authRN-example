db9912e5b7634e8264c7e5fa0be1fd4866dcc216
gerashdo <gerardo.jimenezarguelles@gmail.com>
2023-02-03 13:01:11 -0600

    Start products listing

--- src/context/ProductsContext.tsx
+++ src/context/ProductsContext.tsx
@@ -1,9 +1,10 @@
-import { createContext, useState } from "react";
-import { Product } from "../interfaces/products";
+import { createContext, useEffect, useState } from "react";
+import cafeApi from "../api/cafeApi";
+import { Product, ProductsResponse } from "../interfaces/products";
 
 interface ProductsContextProps {
     products: Product[];
-    // loadProducts: () => Promise<void>;
+    loadProducts: () => Promise<void>;
     // addProduct: ( categoryId: string, productName: string ) => Promise<void>;
     // updateProduct: ( categoryId: string, productName: string, productId: string ) => Promise<void>;
     // deleteProduct: ( id: string ) => Promise<void>;
@@ -17,9 +18,20 @@ export const ProductsProvider = ({ children }: any) => {
 
     const [products, setProducts] = useState<Product[]>([])
 
+    useEffect(() => {
+        loadProducts()
+    }, [])
+    
+    const loadProducts = async() => {
+        const { data } = await cafeApi.get<ProductsResponse>( '/product?limit=10' )
+        // setProducts([ ...products, ...data.products ])
+        setProducts([ ...data.products ])
+    }
+
     return (
         <ProductsContext.Provider value={{
             products,
+            loadProducts,
         }}>
             { children }
         </ProductsContext.Provider>
--- src/screens/ProductsScreen.tsx
+++ src/screens/ProductsScreen.tsx
@@ -1,10 +1,42 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useContext } from 'react'
+import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { ProductsContext } from '../context/ProductsContext'
 
 export const ProductsScreen = () => {
-  return (
-    <View>
-        <Text>ProductsScreen</Text>
-    </View>
-  )
+
+    const { products, loadProducts } = useContext( ProductsContext )
+    
+    return (
+        <View style={ styles.container }>
+            <FlatList 
+                data={ products }
+                keyExtractor={ ( item ) => item._id }
+                renderItem={ ({ item }) => (
+                    <TouchableOpacity
+                        activeOpacity={ 0.7 }
+                    >
+                        <Text style={ styles.productName }>{ item.name }</Text>
+                    </TouchableOpacity>
+                )}
+                ItemSeparatorComponent={ () => (
+                    <View style={ styles.itemSeparator } />
+                )}
+            />
+        </View>
+    )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        marginHorizontal: 20,
+        marginVertical: 10,
+    },
+    productName: {
+        fontSize: 20,
+    },
+    itemSeparator: {
+        borderBottomWidth: 2,
+        borderBottomColor: 'rgba(0,0,0,0.2)',
+        marginVertical: 5,
+    }
+})
